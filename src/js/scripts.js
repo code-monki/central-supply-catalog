c5a9e219ba2de9b4a1ce2e41418a8d281bbf2040
Chuck McKnight <chuckmcknight@pheonic.com>
2021-06-03 20:03:25 -0700

    Converted home page to use Materialize CSS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,29 +1,8 @@
-const disclaimerToggle = document.getElementById('disclaimer-toggle')
-const disclaimer = document.querySelector('.disclaimer')
-const disclaimerContent = document.querySelector('.disclaimer-content')
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav
+  const sideNav = document.querySelector('.sidenav')
+  M.Sidenav.init(sideNav, {})
 
-// toggle the disclaimer pane
-if (disclaimerToggle !== null) {
-  disclaimerToggle.addEventListener('click', (e) => {
-    // let disclaimerContent = document.querySelector('.disclaimer-content')
-
-    // show or hide the content pane
-    if (disclaimerContent !== null) {
-      let displayType = 'block'
-      disclaimerContent.style.display = disclaimerContent.style.display === 'none' ? displayType : 'none'
-    }
-
-    // change the chevron to point up if closed, down if open
-    let icon = document.getElementById('disclaimer-toggle-icon')
-    if (icon !== null) {
-      icon.className = disclaimerContent.style.display === 'none' ? 'fas fa-chevron-up' : 'fas fa-chevron-down'
-    }
-  })
-}
-
-// event handler to detect click outside of disclaimer
-disclaimer.addEventListener('blur', () => {
-  // hide the disclaimer content
-  disclaimerContent.style.display = 'none'
-}, true)
-
+  const modals = document.querySelectorAll('.modal')
+  M.Modal.init(modals, {})
+})
