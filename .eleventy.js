11c0f4af7655ba51db4cb34543859d60a9d93df0
Chuck McKnight <chuckmcknight@pheonic.com>
2021-06-16 17:05:47 -0700

    Removed unneeded method

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const Path = require("path");
 const fs = require('fs')
 const mfrData = require('./src/_data/manufacturers.json')
  
-const currentEnv = process.env.ELEVENTY_ENV;
+const basePath = (process.env.ELEVENTY_ENV === 'prod') ? '/central-supply-catalog' : '/'
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
@@ -12,14 +12,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   
   eleventyConfig.addWatchTarget("./src/scss");
-
-  eleventyConfig.addFilter("relative", function (url) {
-    return Path.join(
-      "./",
-      this.ctx.page.url.split("/").reduce((a, b) => a + (b && "../")),
-      url
-    );
-  });
 
   eleventyConfig.addFilter('costLabel', (cost) => {
     let value = (cost > 999999) ? 
@@ -36,17 +28,6 @@ module.exports = function (eleventyConfig) {
     }
     return res;
   })
-
-  eleventyConfig.addFilter("setRootPath", (rootPath) => {
-    console.log(`currentEnv: [${currentEnv}]`)
-    console.log(`rootPath: [${rootPath}]`)
-    let path = rootPath
-    if (currentEnv === 'prod') {
-      path = (rootPath === '/') ? './' : `../${rootPath}`
-    }    console.log(`Returning path: [${path}]`)
-    return path
-  })
-
 
   eleventyConfig.addShortcode("getAccessory", function(products, sku) {
     let object = products.find(item => item.sku === sku)
@@ -70,7 +51,7 @@ module.exports = function (eleventyConfig) {
   })
 
   return {
-    pathPrefix: "/central-supply-catalog/",
+    pathPrefix: basePath,
     dir: {
       output: "build",
       input: "src",
