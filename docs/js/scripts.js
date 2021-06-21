39b907a8ede9927929dff34f84a57a6574fabe14
Chuck McKnight <chuckmcknight@pheonic.com>
2021-06-21 15:28:19 -0700

    New build

---
+++ docs/js/scripts.js
@@ -0,0 +1,1 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".sidenav");M.Sidenav.init(e,{});e=document.querySelectorAll(".modal");M.Modal.init(e,{})});
