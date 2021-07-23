1674acb07b6636869c4a077a468342a92f5daf74
Chuck McKnight <chuckmcknight@pheonic.com>
2021-07-22 17:33:31 -0700

    Modified buildCategoryCard to simplify HTML/CSS

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const markdownIt = require("markdown-it");
 const mfrData = require("./progdata/manufacturers.json");
 const categoriesData = require("./progdata/categories.json");
 const departmentsData = require("./progdata/departments.json");
-const slugify = require('slugify')
+const slugify = require("slugify");
 
 const basePath = process.env.ELEVENTY_ENV === "prod" ? "/central-supply-catalog" : "";
 const buildDest = process.env.ELEVENTY_ENV === "prod" ? "docs" : "build";
@@ -20,31 +20,42 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setQuietMode(true);
   eleventyConfig.addWatchTarget("./src/scss");
 
+  //-------------------------------------------------------------
+  // Create the cost label with the appropriate units
+  //-------------------------------------------------------------
   eleventyConfig.addFilter("costLabel", (cost) => {
-    let value = ''
+    let value = "";
 
     if (cost > 999999999999) {
-      value = `${cost / 10**12} TCr`
+      value = `${cost / 10 ** 12} TCr`;
     } else if (cost > 999999999) {
-      value = `${cost / 10**9} BCr`
+      value = `${cost / 10 ** 9} BCr`;
     } else if (cost > 999999) {
-      value = `${cost / 10**6} MCr`
+      value = `${cost / 10 ** 6} MCr`;
     } else if (cost > 999) {
-      value = `${cost / 10**3} KCr`
+      value = `${cost / 10 ** 3} KCr`;
     } else {
-      value = `${cost} Cr`
+      value = `${cost} Cr`;
     }
-   return value;
+    return value;
   });
 
+  //-------------------------------------------------------------
+  // Render the incoming content Markdown fragment as HTML
+  //-------------------------------------------------------------
   eleventyConfig.addFilter("markdown", (content) => {
     let text = md.render(content);
     return text;
   });
 
+  //-------------------------------------------------------------
   // create shortcode to extract summary
+  //-------------------------------------------------------------
   eleventyConfig.addShortcode("summary", (article) => extractSummary(article));
 
+  //-------------------------------------------------------------
+  // Get the categories
+  //-------------------------------------------------------------
   eleventyConfig.addShortcode("getCategories", function () {
     let text = "<div><ul>";
     categoriesData.forEach((item) => {
@@ -54,6 +65,9 @@ module.exports = function (eleventyConfig) {
     return text;
   });
 
+  //-------------------------------------------------------------
+  // Get the manufacturer name
+  //-------------------------------------------------------------
   eleventyConfig.addShortcode("getMfr", function (mfrId) {
     const mfr = mfrData.find((obj) => obj.mfrId === mfrId);
     let res = "N/A";
@@ -63,30 +77,33 @@ module.exports = function (eleventyConfig) {
     return res;
   });
 
+  //-------------------------------------------------------------
   // Get Accessory
+  //-------------------------------------------------------------
   eleventyConfig.addShortcode("getAccessory", function (sku) {
     // get the parent department
-    let key = sku.slice(0, 3) + "-000"
+    let key = sku.slice(0, 3) + "-000";
 
     // get the name of the data file
-    let dataSrc = departmentsData.find((item) => 
-      ( item.id === key) ? item.data : ''
-    )
+    let dataSrc = departmentsData.find((item) => (item.id === key ? item.data : ""));
 
     let object = null;
 
     // read the data file
-    if (dataSrc !== '') {
-      let typeProducts = JSON.parse(fs.readFileSync(dataSrc.data))
-      object = typeProducts.find((item) => item.sku === sku)
+    if (dataSrc !== "") {
+      let typeProducts = JSON.parse(fs.readFileSync(dataSrc.data));
+      object = typeProducts.find((item) => item.sku === sku);
     }
 
     let text = `<p>No accessories available</p>`;
 
     if (object !== undefined && object !== null) {
-      let imgURL = (object.image === '' || object.image === null) ? `/${basePath}img/products/no-image.png` : `/${basePath}src/img/products/${sku}.png`;
+      let imgURL =
+        object.image === "" || object.image === null
+          ? `/${basePath}img/products/no-image.png`
+          : `/${basePath}src/img/products/${sku}.png`;
       let pageURL = `../products/${sku}/`;
-      
+
       text = `
       <div class="row accessory-row">
         <div class="col s3 m2 l1 accessory-item-img">
@@ -102,26 +119,32 @@ module.exports = function (eleventyConfig) {
       </div>
       <div class="divider"></div>
 
-      `
+      `;
     }
 
     return text;
   });
 
+  //-------------------------------------------------------------
   // build category cards for home page
+  //-------------------------------------------------------------
   eleventyConfig.addShortcode("buildCategoryCards", () => {
-    let text = `<div class="category-cards container">
-                <div class="row">`;
+    let text = `<div class="categories-container container">
+                  <div class="row">`;
     categoriesData.forEach((category) => {
       text += buildCategoryCard(category);
     });
 
-    text += "</div></div>";
+    text += `
+      </div>
+    </div>`;
 
     return text;
   });
 
+  //-------------------------------------------------------------
   // Build Department cards
+  //-------------------------------------------------------------
   eleventyConfig.addShortcode("buildDepartmentCards", () => {
     let text = "";
     departmentsData.forEach((dept) => {
@@ -158,63 +181,63 @@ module.exports = function (eleventyConfig) {
   };
 };
 
+//-------------------------------------------------------------
+// Construct one category card
+//-------------------------------------------------------------
 const buildCategoryCard = (category) => {
-  let text = `<div class="col s12 m6 card-container">
-                <div class="card-panel black category-card">
-                  <div class="category-card-content">
-                    <a href="#${urlSafe(category.label)}-modal" class="modal-trigger btn big-button black red-text">
-                      ${category.label}
-                      <img src="img/${urlSafe(category.label)}.svg" alt="${category.label}">
-                    </a>
-                  </div>
-                </div>
+  let text = `<div class="cat-btn col s12 m4 l3 offset-l0 xl4">
+                <a href="#${urlSafe(category.label)}-modal" class="modal-trigger btn big-button black red-text flow-text">
+                  <img src="img/${urlSafe(category.label)}.svg" alt="${category.label}"><br>${category.label}
+                </a>
               </div>
               
               <div id="${urlSafe(category.label)}-modal" class="modal card-modal">
-              <div class="modal-content">
-        
-                <a class="modal-close">
-                  <i class="material-icons right">close</i>
-                </a>
-                
-                <h6>${category.label}</h6>
-                
-                <div class="menu-lists">
-                  <div>
-                    <ul>`;
-
+                <div class="modal-content">
+                  <a class="modal-close"><i class="material-icons right">close</i></a>
+                  
+                  <h6>${category.label}</h6>
+                  
+                  <div class="menu-lists">
+                    <div>
+                      <ul>`;
   category.departments.forEach((dept) => {
     let o = departmentsData.find((m) => m.id === dept);
 
     if (o !== undefined) {
-      text += `                      <li><a href="${basePath}/departments/${urlSafe(o.label)
-        .toLowerCase()
-        .replace(" ", "-")}/">${o.label}</a></li>`;
+      text += `<li><a href="${basePath}/departments/${urlSafe(o.label)
+        .toLowerCase().replace(" ", "-")}/">${o.label}</a></li>`
     } else {
       // FIXME
-      // console.log(`Undefined: ${dept}`);
+      // console.log(`Undefined department: ${dept}`)
     }
-  });
+  })
 
-  text += `         </ul>
-                  </div>
-                </div>
-        
-              </div>
-            </div>`;
+  text += `</ul>
+  </div>
+  </div>
+  
+  </div>
+  </div>`
 
   return text;
 };
 
+//-------------------------------------------------------------
+// Strip out any special characters from the URL
+//-------------------------------------------------------------
 const urlSafe = (text) => {
-  let txt = text.replace(/[\,\"\.\*\@\!\?\<\>\&\^\%\$\#\~\`]/g, "")
-                .replace(" ", "-")
-                .toLowerCase()
-  
+  let txt = text
+    .replace(/[\,\"\.\*\@\!\?\<\>\&\^\%\$\#\~\`]/g, "")
+    .replace(" ", "-")
+    .toLowerCase();
+
   return txt;
 };
 
-function extractSummary(text) {
+//-------------------------------------------------------------
+// Extract marked summary from text.
+//-------------------------------------------------------------
+const extractSummary = (text) => {
   let summary = null;
 
   // The start and end separators to try and match to extract the summary
