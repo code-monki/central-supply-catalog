6ce339bb3b31da4367aafece39dc312b1ca4f1e0
Chuck McKnight <chuckmcknight@pheonic.com>
2021-08-11 20:07:17 -0700

    New build

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,252 @@
-"use strict";function _slicedToArray(t,e){return _arrayWithHoles(t)||_iterableToArrayLimit(t,e)||_unsupportedIterableToArray(t,e)||_nonIterableRest()}function _nonIterableRest(){throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function _unsupportedIterableToArray(t,e){if(t){if("string"==typeof t)return _arrayLikeToArray(t,e);var a=Object.prototype.toString.call(t).slice(8,-1);return"Map"===(a="Object"===a&&t.constructor?t.constructor.name:a)||"Set"===a?Array.from(t):"Arguments"===a||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(a)?_arrayLikeToArray(t,e):void 0}}function _arrayLikeToArray(t,e){(null==e||e>t.length)&&(e=t.length);for(var a=0,n=new Array(e);a<e;a++)n[a]=t[a];return n}function _iterableToArrayLimit(t,e){var a=null==t?null:"undefined"!=typeof Symbol&&t[Symbol.iterator]||t["@@iterator"];if(null!=a){var n,r,o=[],c=!0,l=!1;try{for(a=a.call(t);!(c=(n=a.next()).done)&&(o.push(n.value),!e||o.length!==e);c=!0);}catch(t){l=!0,r=t}finally{try{c||null==a.return||a.return()}finally{if(l)throw r}}return o}}function _arrayWithHoles(t){if(Array.isArray(t))return t}var hostName="localhost"===document.location.hostname?"":"https://cmcknight.github.io/central-supply-catalog",cartKey="csc-cart",unitMultiplier={Cr:0,KCr:3,MCr:6,BCr:9,TCr:12},setUnitLabel=function(t){return 999999999999<t?"".concat(t/Math.pow(10,12)," TCr"):999999999<t?"".concat(t/Math.pow(10,9)," BCr"):999999<t?"".concat(t/Math.pow(10,6)," MCr"):999<t?"".concat(t/Math.pow(10,3)," KCr"):"".concat(t," Cr")},addItem=function(){var t=localStorage.getItem(cartKey),e=document.getElementById("prod-img").src,a=_slicedToArray(document.getElementById("unit-price").textContent.split(" "),2),n=a[0],r=a[1],o=parseInt(n)*Math.pow(10,unitMultiplier[r]),a=document.getElementById("product-name").textContent,n=parseInt(document.getElementById("product-qty").value),c=document.getElementById("product-name").getAttribute("data-sku"),r=(t=null==t?[]:JSON.parse(t)).find(function(t){return t.sku===c});null==r?t.push({sku:c,qty:n,name:a,unitPrice:o,image:e}):r.qty+=n,t=t.sort(function(t,e){return t.name<e.name?-1:t.name>e.name?1:0}),localStorage.setItem(cartKey,JSON.stringify(t)),updateCartBadge(),M.toast({html:"Item added to cart",displayLength:8e3})},removeItem=function(e){var t=(t=JSON.parse(localStorage.getItem(cartKey))).filter(function(t){return t.sku!==e});localStorage.setItem(cartKey,JSON.stringify(t)),updateCartUI(),updateCartBadge()},updateItemQty=function(e,t){var a=JSON.parse(localStorage.getItem(cartKey)),n=a.find(function(t){return t.sku===e});t<0&&(t=0),null===n|void 0===n?console.log("Sku: ".concat(e," is not in cart")):n.qty=t,localStorage.setItem(cartKey,JSON.stringify(a)),updateCartUI()},updateCartUI=function(){var t=JSON.parse(localStorage.getItem(cartKey)),e=document.querySelector(".cart-items-container"),a=document.querySelector(".cart-total"),n="",r=0;null==t||0===t.length?(console.log("No items in cart"),n='<div class="row">\n              <h5 class="center">No items in cart</h5>\n            </div>'):t.forEach(function(t){n+='\n      <div class="row product-row">\n\n        <div class="prod-img col s3 m2 l2">\n          <a href="#">\n            <img src="'.concat(t.image,'" class="responsive-img" alt="').concat(t.name,'">\n          </a>\n        </div>\n\n        <div class="col s9 m10">\n          <div class="row">\n\n            <div class="prod-title col s8">\n                <a href="{{ ../products/').concat(t.name,' | url }}" data-sku="').concat(t.sku,'" class="item-name">').concat(t.name,'</a>\n            </div>\n\n            <div class="prod-total col s4 right-align right">\n              ').concat(setUnitLabel(t.qty*t.unitPrice),'\n            </div>\n\n          </div>\n\n          <div class="row">\n            <div class="col s12 prod-qty">\n                  <button><i class="fa fa-minus subtract-btn"></i></button>\n                  <input type="number" class="qty" value="').concat(t.qty,'">\n                  <button><i class="fa fa-plus add-btn"></i></button>\n                  <button class="remove-item"><i class="fa fa-trash"></i></button>\n            </div>\n          </div>\n        </div>\n\n        </div>\n\n      </div>\n      '),r+=t.unitPrice*t.qty}),0===t.length?a.innerHTML="":a.innerHTML="Total: ".concat(setUnitLabel(r)),e.innerHTML=n},cartItemsList=document.querySelector(".cart-items-container");null!=cartItemsList&&(cartItemsList.addEventListener("change",function(t){var e;t.preventDefault,"qty"===t.target.className&&(e=t.target.parentNode.parentNode.querySelector(".qty"),e=Number(e.value),t=t.target.parentNode.parentNode.parentNode.querySelector(".item-name").dataset.sku,updateItemQty(t,e))}),cartItemsList.addEventListener("click",function(t){t.preventDefault;var e=t.target.parentNode.parentNode.querySelector(".qty"),a=Number(e.value),e=t.target.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector(".item-name").dataset.sku;t.target.className.includes("fa-minus")?updateItemQty(e,a-1):t.target.className.includes("fa-plus")?updateItemQty(e,a+1):t.target.className.includes("fa-trash")&&removeItem(e)}));var removeAllItemsBtn=document.getElementById("empty-cart");null!=removeAllItemsBtn&&removeAllItemsBtn.addEventListener("click",function(){localStorage.setItem(cartKey,JSON.stringify([])),document.querySelector(".cart-total").textContent="",updateCartUI(),updateCartBadge()});var updateCartBadge=function(){var t=document.getElementById("cart-badge"),e=JSON.parse(localStorage.getItem(cartKey)).length;0===e?t.style.display="none":(t.textContent=e,t.style.display="block")};document.addEventListener("DOMContentLoaded",function(){var t=document.querySelector(".sidenav");M.Sidenav.init(t,{});t=document.querySelectorAll(".modal");M.Modal.init(t,{});t=document.getElementById("add-to-cart");null!=t&&t.addEventListener("click",addItem),updateCartBadge(),window.location.href.includes("shopping-cart")&&updateCartUI()});
+const hostName = document.location.hostname === "localhost" ? "" : "https://cmcknight.github.io/central-supply-catalog";
+
+// ----- Shopping Cart-related functionality -----
+const cartKey = "csc-cart"; // key for localStorage shopping cart
+
+// powers of 10 multipliers
+const unitMultiplier = { Cr: 0, KCr: 3, MCr: 6, BCr: 9, TCr: 12 };
+
+// Apply units to values
+const setUnitLabel = (value) => {
+  let text = "";
+
+  if (value > 999999999999) {
+    text = `${value / 10 ** 12} TCr`;
+  } else if (value > 999999999) {
+    text = `${value / 10 ** 9} BCr`;
+  } else if (value > 999999) {
+    text = `${value / 10 ** 6} MCr`;
+  } else if (value > 999) {
+    text = `${value / 10 ** 3} KCr`;
+  } else {
+    text = `${value} Cr`;
+  }
+  return text;
+};
+
+// ----------------- add item to cart -----------------
+const addItem = () => {
+  // retrieve the cart from localStorage
+  let cart = localStorage.getItem(cartKey);
+
+  // get image src attribute
+  const productImage = document.getElementById("prod-img").src;
+
+  // get the price and units
+  const [price, units] = document.getElementById("unit-price").textContent.split(" ");
+  const unitPrice = parseInt(price) * 10 ** unitMultiplier[units];
+
+  // get the product name
+  const productName = document.getElementById("product-name").textContent;
+
+  // get the quantity
+  const productQty = parseInt(document.getElementById("product-qty").value);
+
+  // get the sku
+  const productSku = document.getElementById("product-name").getAttribute("data-sku");
+
+  cart = cart === null || cart === undefined ? (cart = []) : (cart = JSON.parse(cart));
+
+  // attempt to get the item from the cart
+  let cartProd = cart.find((s) => s.sku === productSku);
+
+  if (cartProd === null || cartProd === undefined) {
+    // product not in cart, add to cart
+    cart.push({ sku: productSku, qty: productQty, name: productName, unitPrice: unitPrice, image: productImage });
+  } else {
+    // product is already in cart, update quantity
+    cartProd.qty += productQty;
+  }
+
+  cart = cart.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
+
+  // save cart to localStorage
+  localStorage.setItem(cartKey, JSON.stringify(cart));
+
+  // update shopping cart badge
+  updateCartBadge();
+
+  M.toast({ html: "Item added to cart", displayLength: 8000 });
+};
+
+// ----------------- remove item from cart ----------------------
+const removeItem = (sku) => {
+  let cart = JSON.parse(localStorage.getItem(cartKey));
+  cart = cart.filter((item) => item.sku !== sku);
+  localStorage.setItem(cartKey, JSON.stringify(cart));
+  updateCartUI();
+  updateCartBadge();
+};
+
+// ---------------- modify quantity of item ----------------------
+const updateItemQty = (sku, qty) => {
+  let cart = JSON.parse(localStorage.getItem(cartKey));
+  let item = cart.find((s) => s.sku === sku);
+  if (qty < 0) qty = 0;
+
+  if ((item === null) | (item === undefined)) {
+    console.log(`Sku: ${sku} is not in cart`);
+  } else {
+    item.qty = qty;
+  }
+  localStorage.setItem(cartKey, JSON.stringify(cart));
+  updateCartUI();
+};
+
+// -------------------- update the cart ui -------------------------
+const updateCartUI = () => {
+  let cart = JSON.parse(localStorage.getItem(cartKey));
+  let itemContainer = document.querySelector(".cart-items-container");
+  let cartTotal = document.querySelector(".cart-total");
+  let text = "";
+  let total = 0;
+
+  if (cart === null || cart === undefined || cart.length === 0) {
+    console.log("No items in cart");
+    text = `<div class="row">
+              <h5 class="center">No items in cart</h5>
+            </div>`;
+  } else {
+    cart.forEach((item) => {
+      text += `
+      <div class="row product-row">
+
+        <div class="prod-img col s3 m2 l2">
+          <a href="#">
+            <img src="${item.image}" class="responsive-img" alt="${item.name}">
+          </a>
+        </div>
+
+        <div class="col s9 m10">
+          <div class="row">
+
+            <div class="prod-title col s8">
+                <a href="{{ ../products/${item.name} | url }}" data-sku="${item.sku}" class="item-name">${item.name}</a>
+            </div>
+
+            <div class="prod-total col s4 right-align right">
+              ${setUnitLabel(item.qty * item.unitPrice)}
+            </div>
+
+          </div>
+
+          <div class="row">
+            <div class="col s12 prod-qty">
+                  <button><i class="fa fa-minus subtract-btn"></i></button>
+                  <input type="number" class="qty" value="${item.qty}">
+                  <button><i class="fa fa-plus add-btn"></i></button>
+                  <button class="remove-item"><i class="fa fa-trash"></i></button>
+            </div>
+          </div>
+        </div>
+
+        </div>
+
+      </div>
+      `;
+      total += item.unitPrice * item.qty;
+    });
+  }
+
+  if (cart.length === 0) {
+    // cart is empty, no display
+    cartTotal.innerHTML = "";
+  } else {
+    // set cart total
+    cartTotal.innerHTML = `Total: ${setUnitLabel(total)}`;
+  }
+
+  itemContainer.innerHTML = text;
+};
+
+// --------- set up listener for click events on cart items --------------
+const cartItemsList = document.querySelector(".cart-items-container");
+if (cartItemsList !== null && cartItemsList !== undefined) {
+  // event for manually changing quantity
+  cartItemsList.addEventListener("change", (e) => {
+    e.preventDefault;
+    if (e.target.className === "qty") {
+      let qtyNode = e.target.parentNode.parentNode.querySelector(".qty");
+      let qty = Number(qtyNode.value);
+      let sku = e.target.parentNode.parentNode.parentNode.querySelector(".item-name").dataset.sku;
+      updateItemQty(sku, qty);
+    }
+  });
+
+  // event for handling button clicks
+  cartItemsList.addEventListener("click", (e) => {
+    e.preventDefault;
+    // console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.item-name').dataset.sku)
+
+    let qtyNode = e.target.parentNode.parentNode.querySelector(".qty");
+    let qty = Number(qtyNode.value);
+
+    let sku = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector(".item-name").dataset.sku;
+
+    if (e.target.className.includes("fa-minus")) {
+      // update the cart
+      updateItemQty(sku, qty - 1);
+    } else if (e.target.className.includes("fa-plus")) {
+      // update the cart
+      updateItemQty(sku, qty + 1);
+    } else if (e.target.className.includes("fa-trash")) {
+      removeItem(sku);
+    }
+  });
+}
+
+// ------------------ Remove all items from cart ----------------------
+const removeAllItemsBtn = document.getElementById("empty-cart");
+if (removeAllItemsBtn !== null && removeAllItemsBtn !== undefined) {
+  removeAllItemsBtn.addEventListener("click", () => {
+    localStorage.setItem(cartKey, JSON.stringify([]));
+    document.querySelector(".cart-total").textContent = "";
+    updateCartUI();
+    updateCartBadge();
+  });
+}
+
+// ---- End of Shopping Cart-related Functionality --------
+
+// ---------- Manage Cart Count Badge ---------------------
+
+const updateCartBadge = () => {
+  const cartCountSpan = document.getElementById("cart-badge");
+  let cartCount = JSON.parse(localStorage.getItem(cartKey)).length;
+
+  if (cartCount === 0) {
+    cartCountSpan.style.display = "none";
+  } else {
+    cartCountSpan.textContent = cartCount;
+    cartCountSpan.style.display = "block";
+  }
+};
+
+// --------- initialize MaterializeCSS components ---------
+document.addEventListener("DOMContentLoaded", function () {
+  // sidenav
+  const sideNav = document.querySelector(".sidenav");
+  M.Sidenav.init(sideNav, {});
+
+  // modal cards
+  const modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals, {});
+
+  // set listener on product page add button
+  const addItemBtn = document.getElementById("add-to-cart");
+  if (addItemBtn !== null && addItemBtn !== undefined) {
+    addItemBtn.addEventListener("click", addItem);
+  }
+
+  // update the cart badge
+  updateCartBadge();
+
+  // update shopping cart ui if on shopping cart page
+  if (window.location.href.includes("shopping-cart")) {
+    updateCartUI();
+  }
+});
