e8196f9b368654272e26675e29f776ccfe4abab2
Chuck McKnight <chuckmcknight@pheonic.com>
2021-08-18 13:13:12 -0700

    Added passthrough copy for sw.js

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/_data/searchindex.idx");
+  eleventyConfig.addPassthroughCopy("src/sw.js");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.setQuietMode(true);
   eleventyConfig.addWatchTarget("./src/scss");
