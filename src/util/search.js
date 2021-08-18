c6aee3cc59282eea35546e876f8865ad81fc06a8
Chuck McKnight <chuckmcknight@pheonic.com>
2021-08-18 10:58:28 -0700

    Adding search index builder to utilities for dev

---
+++ src/util/search.js
@@ -0,0 +1,27 @@
+const miniSearch = require('minisearch')
+const fs = require('fs')
+const inputFiles = JSON.parse(fs.readFileSync("src/_data/products-manifest.json"))
+
+let idCounter = 0
+
+let ms = new miniSearch({
+  fields: ['name', 'type', 'subtype', 'description'],
+  storeFields: ['sku', 'name', 'description', 'cost']
+})
+
+inputFiles.forEach(file => {  
+  // get the products from the file
+  let products = JSON.parse(fs.readFileSync(`src/_data/${file}.json`))
+
+  // build search index object and add to search index
+  products.forEach(product => {
+    product.id = idCounter++
+    ms.add(product)
+  })
+})
+
+let searchTerm = 'gas and torch cutting'
+let options = (searchTerm.includes(' and ')) ? { combineWith: 'AND'} : {}
+
+fs.writeFileSync('searchindex.idx', JSON.stringify(ms))
+console.log(ms.search(searchTerm, options ));
