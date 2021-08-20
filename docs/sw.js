65c56278f22e38b726af08f02469449c35abf3e4
Chuck McKnight <chuckmcknight@pheonic.com>
2021-08-20 15:32:07 -0700

    Munging sw path for GH Pages

--- docs/sw.js
+++ docs/sw.js
@@ -1,12 +1,18 @@
 const version = 2;
 const cacheName = `csc-cache-v${version}`;
-const indexFilename = `/${location.pathname.match("((?:/central-supply-catalog/)?)")[0]}_data/searchindex.idx`;
+
+const indexFilename = `${
+  (location.pathname.match("((?:/central-supply-catalog/)?)")[0]) 
+    ? location.pathname[0] 
+    : '/'
+  }_data/searchindex.idx`;
+// console.log(`${location.host}${location.pathname[0]}${indexFilename}`);
 console.log(`indexFilename: ${indexFilename}`);
 const preCache = [indexFilename]
 
 this.addEventListener('install', function(ev) {
   ev.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(preCache))
+    caches.open(cacheName).then(cache => cache.add(indexFilename))
   );
 });
 
