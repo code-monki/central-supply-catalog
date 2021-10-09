4a4c6a67756e9be866b862e3627a675ca9c330da
Chuck McKnight <chuckmcknight@pheonic.com>
2021-10-09 15:56:27 -0700

    Corrected search index builder

--- src/util/search.js
+++ src/util/search.js
@@ -1,40 +1,84 @@
 const miniSearch = require('minisearch')
-const fs = require('fs')
-const inputFiles = JSON.parse(fs.readFileSync("progdata/products/products-manifest.json"))
+const fs = require('fs');
+const path = require("path");
 
-let idCounter = 0
+const getProductFiles = function(dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function(file) {
+    let fn = path.join(dirPath, file);
+    (fs.statSync(fn).isDirectory()) ?
+      arrayOfFiles = getProductFiles(fn, arrayOfFiles) :
+      arrayOfFiles.push(path.join(dirPath, "/", file));
+  });
+
+  return arrayOfFiles;
+}
+
+let arrayOfFiles;
+const inputFiles = 
+  getProductFiles(path.join('src', '_data'), arrayOfFiles)
+      .filter(file => path.extname(file) === '.json');
+
+// let idCounter = 0
 
 let ms = new miniSearch({
   fields: [
-    'sku',
-    'category',
-    'type', 
-    'subtype', 
-    'name', 
-    'description',
-    'cost',
-    'mass',
-    'size',
-    'techLevel',
-    'qrebs',
-    'tags'
-  ],
+    "sku",
+    "name",
+    "description",
+    "cost",
+],
   storeFields: ['sku', 'name', 'description', 'cost']
-})
+});
 
-inputFiles.forEach(file => {  
-  // get the products from the file
-  let products = JSON.parse(fs.readFileSync(`src/_data/${file}.json`))
+const products = inputFiles.flatMap((file) => JSON.parse(fs.readFileSync(file)));
 
-  // build search index object and add to search index
-  products.forEach(product => {
-    product.id = idCounter++
-    ms.add(product)
-  })
-})
+// add id field
+// let counter = 0;
+products.forEach(product => product.id = products.indexOf(product))
 
-let searchTerm = 'gas and torch cutting'
-let options = (searchTerm.includes(' and ')) ? { combineWith: 'AND'} : {}
+// console.log(products);
 
-fs.writeFileSync('src/_data/searchindex.idx', JSON.stringify(ms))
-console.log(ms.search(searchTerm, options ));
+// Add all of the products into the search map
+ms.addAll(products);
+
+fs.writeFileSync('src/_data/searchindex.idx', JSON.stringify(products))
+
+// let jsonIdx = fs.readFileSync('src/_data/searchindex.idx', 'utf8');
+
+// let ms2 = new miniSearch.loadJSON(jsonIdx, {
+//   fields: [ 'sku', 'category', 'type', 'subtype', 'name', 'description', 'cost',
+//             'mass', 'size', 'techLevel', 'qrebs', 'tags' ],
+//   storeFields: ['sku', 'name', 'description', 'cost']
+// });
+
+
+// // console.log(`ms is ${(Array.isArray(ms)) ? "" : "not"})`)
+// let searchTerm = 'portal'
+// let options = (searchTerm.includes(' and ')) ? { combineWith: 'AND'} : {}
+// let res = ms2.search(searchTerm, options);
+// res.forEach(result => console.log(result));
+
+// let testJSON = fs.readFileSync('src/_data_searchindex.idx', {encoding: 'utf8', flag: 'r'});
+
+// let msTest = new miniSearch({
+//   fields: [
+//     'sku',
+//     'category',
+//     'type', 
+//     'subtype', 
+//     'name', 
+//     'description',
+//     'cost',
+//     'mass',
+//     'size',
+//     'techLevel',
+//     'qrebs',
+//     'tags'
+//   ],
+//   storeFields: ['sku', 'name', 'description', 'cost']
+// });
+// msTest.loadJSON(testJson)
