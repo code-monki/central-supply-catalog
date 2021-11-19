5d781c56076158820ee8afb7aebe8750b31200a5
Chuck McKnight <chuckmcknight@pheonic.com>
2021-11-18 16:20:41 -0800

    Added deprecation verbiage
    Commented out file write

--- src/util/burster.js
+++ src/util/burster.js
@@ -1,12 +1,15 @@
 /**
  * burster.js
- * 
+ *
  * This application takes a file of products and splits it into individual product
  * files based on the SKU. The new files will have names reflecting the SKU and will
  * continue to be individual JSON objects.
- * 
+ *
  * Usage:   node burster.js <data directory>
- * 
+ *
+ *
+ * NOTE: This application is no longer needed and will destroy the contents of the
+ * various products directory if run.
  *
  */
 
@@ -14,9 +17,8 @@ const FS = require('fs');
 const PATH = require('path');
 const PROCESS = require('process');
 
-
 // retrieve data directory from command line
-const dataDirectory = process.argv[2];
+const dataDirectory = PROCESS.argv[2];
 
 // console.log(dataDirectory);
 
@@ -25,14 +27,15 @@ const dataDirectory = process.argv[2];
 const directoryList = FS.readdirSync(dataDirectory);
 
 // Process data files in each directory
-directoryList.forEach(category => {
+directoryList.forEach((category) => {
   let targetDir = PATH.join(dataDirectory, category);
   let dataFile = JSON.parse(FS.readFileSync(PATH.join(targetDir, 'data.json')));
 
   // loop through the array of objects and write each to its own file
-  dataFile.forEach(product => {
+  dataFile.forEach((product) => {
     let fileName = PATH.join(targetDir, `${product.sku}.json`);
-    FS.writeFileSync(fileName, JSON.stringify(product, null, '  '));
-  })
-  
-})
+
+    // commented out for safety reasons. See NOTE above
+    // FS.writeFileSync(fileName, JSON.stringify(product, null, '  '));
+  });
+});
